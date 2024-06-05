6bea87006a1217df3f8ed97b625c67770678388d
Mateusz Śródka <mateusz.srodka@gmail.com>
2024-06-05 04:15:31 +0200

    Game ready state

--- Color.py
+++ Color.py
@@ -2,6 +2,11 @@ from enum import Enum
 
 
 class Color(Enum):
+    primary = (156, 39, 176)
+    light_primary = (225, 190, 231)
+    primary_text = (33, 33, 33)
+    secondary_text = (117, 117, 117)
+    accent = (124, 77, 255)
     white = (255, 255, 255)
     black = (0, 0, 0)
     gray = (128, 128, 128)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import random
 import time
 
@@ -9,7 +10,13 @@ from CardColor import CardColor
 from Color import Color
 
 pygame.init()
+pygame.mixer.init()
 
+s_dir = 'sound'
+pygame.mixer.music.load(os.path.join(s_dir, 'bkg.mp3'))
+match_sound_effect = pygame.mixer.Sound(os.path.join(s_dir, 'match.mp3'))
+
+pygame.mixer.music.play(-1)
 EMPTY = 0
 MATCHED = 1
 CHECKED = 2
@@ -45,14 +52,16 @@ second_guess = False
 first_guess_num = 0
 second_guess_num = 0
 matched_pairs = 0
+restart_var = False
 turns = 0
 
+is_game_over = False
+
 # create screen
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
+screen = pygame.display.set_mode([WIDTH, HEIGHT], pygame.RESIZABLE)
 pygame.display.set_caption("Memory Game")
 title_font = pygame.font.Font('Roboto-Regular.ttf', 56)
 small_font = pygame.font.Font('Roboto-Regular.ttf', 40)
-pygame.init()
 
 
 def log_error(msg):
@@ -60,21 +69,28 @@ def log_error(msg):
 
 
 def log_debug(msg):
-    print(f"[DEBUG] {msg}")
+    if debug:
+        print(f"[DEBUG] {msg}")
 
 
 def draw_backgrounds():
     global turns
     global matched_pairs
-    top_menu = pygame.draw.rect(screen, Color.black.value, [0, 0, WIDTH, 100])
+    top_menu = pygame.draw.rect(screen, Color.primary.value, [0, 0, WIDTH, 100])
     title_text = title_font.render('The Memory Game!', True, Color.white.value)
     screen.blit(title_text, (55, 20))
-    board_space = pygame.draw.rect(screen, Color.gray.value, [0, 100, WIDTH, HEIGHT - 200], 0)
-    bottom_menu = pygame.draw.rect(screen, Color.black.value, [0, HEIGHT - 100, WIDTH, 100], 0)
+    board_space = pygame.draw.rect(screen, Color.light_primary.value, [0, 100, WIDTH, HEIGHT - 200], 0)
+    bottom_menu = pygame.draw.rect(screen, Color.primary.value, [0, HEIGHT - 100, WIDTH, 100], 0)
+    restart_button = pygame.draw.rect(screen, Color.accent.value, [415, HEIGHT - 85, 145, 60], 0)
+    restart_button_frame = pygame.draw.rect(screen, Color.primary_text.value, [415, HEIGHT - 85, 145, 60], 3, 4)
     turn_text = small_font.render(f"Turns: {turns}", True, Color.white.value)
     matches_text = small_font.render(f"Matches: {matched_pairs}", True, Color.white.value)
+    restart_text = small_font.render('Restart', True, Color.white.value)
     screen.blit(turn_text, (20, HEIGHT - 80))
     screen.blit(matches_text, (200, HEIGHT - 80))
+    screen.blit(restart_text, (420, HEIGHT - 80))
+
+    return restart_button
 
 
 def draw_grid():
@@ -156,6 +172,32 @@ def is_match(first, second):
     return False
 
 
+def restart_game():
+    global options_list
+    global used
+    global spaces
+    global new_grid
+    global matched_pairs
+    global first_guess
+    global second_guess
+    global correct
+    global is_game_over
+    global turns
+    options_list = []
+    used = []
+    spaces = []
+    new_grid = True
+    turns = 0
+    matched_pairs = 0
+    first_guess = False
+    second_guess = False
+    correct = [[0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0]]
+    is_game_over = False
+
+
 # main game loop
 running = True
 while running:
@@ -167,11 +209,12 @@ while running:
         # spaces.sort()
         print(spaces)
         new_grid = False
-    draw_backgrounds()
+    restart = draw_backgrounds()
     grid = draw_grid()
 
     if first_guess and second_guess:
         if is_match(first_guess_num, second_guess_num):
+            pygame.mixer.Sound.play(match_sound_effect)
             log_debug("IT'S A MATCH")
         else:
             pygame.time.delay(1000)
@@ -185,6 +228,11 @@ while running:
             if event.key == pygame.K_j:
                 for line in correct:
                     log_debug(line)
+            if event.key == pygame.K_q:
+                if not debug:
+                    debug = True
+                else:
+                    debug = False
 
         if event.type == pygame.QUIT:
             running = False
@@ -201,6 +249,8 @@ while running:
                     second_guess_num = i
                     log_debug(f"SECOND {i}")
                     update_checked_card(i, 2)
+            if restart.collidepoint(event.pos):
+                restart_var = True
 
     if first_guess:
         update_checked_card(first_guess_num, 2)
@@ -208,6 +258,15 @@ while running:
     if second_guess:
         update_checked_card(second_guess_num, 2)
     grid = draw_grid()
+
+    if matched_pairs == NUMBER_OF_PAIRS:
+        is_game_over = True
+        winner = pygame.draw.rect(screen, Color.accent.value, [10, HEIGHT - 350, WIDTH - 20, 80], 0, 3)
+        winner_text = title_font.render("You won! Great job!", True, Color.white.value)
+        screen.blit(winner_text, (55, HEIGHT - 345))
+    if restart_var:
+        restart_game()
+        restart_var = False
 
     pygame.display.flip()
 pygame.quit()
